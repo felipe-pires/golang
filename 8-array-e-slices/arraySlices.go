3f72cbfc2fcc097b268112d900551bae0127465e
Felipe dos Santos <felipedossantospires01@gmail.com>
2023-02-20 17:42:24 -0300

    arrays internos

--- 8-array-e-slices/arraySlices.go
+++ 8-array-e-slices/arraySlices.go
@@ -30,4 +30,25 @@ func main() {
 
 	fmt.Print(reflect.TypeOf(slice))
 	fmt.Print(reflect.TypeOf(array3))
+	fmt.Println()
+	fmt.Println()
+
+	//Arrays Internos
+
+	// arrays sao a base dos slices - quando um novo elemento é adiconado
+	//a um slice um array é descartado e criado um novo que copia o anterior e  adiciona o novo item
+
+	slice2 := make([]int, 10, 15) //tipo ,tamanho inicial, capacidade
+	fmt.Println(slice2)
+
+	fmt.Println(len(slice2)) //func length
+	fmt.Println(cap(slice2)) // capacidade atual
+
+	//quando a capacidade é ultrapassada é criado um novo array interno com o dobro do anterior
+	slice2 = append(slice2, 1, 2, 3, 4, 5, 6)
+	fmt.Println(slice2)
+
+	fmt.Println(len(slice2))
+	fmt.Println(cap(slice2))
+
 }
