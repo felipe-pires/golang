2c3549efc30a011112dc367b0118ccb6ba0fb261
Felipe dos Santos <felipedossantospires01@gmail.com>
2023-02-20 18:13:27 -0300

    switch

--- 10-estruturas-de-controle/controle.go
+++ 10-estruturas-de-controle/controle.go
@@ -8,4 +8,27 @@ func main() {
 	if numero := 10; numero > 0 {
 		fmt.Println("if init")
 	}
+
+	//switch
+
+	num := 3
+
+	switch num {
+	case 1:
+		fmt.Println("switch")
+	case 2:
+		fmt.Println("switch")
+	default:
+		fmt.Println("switch")
+	}
+
+	switch {
+	case num == 1:
+		fmt.Println("switch")
+	case num == 2:
+		fmt.Println("switch")
+	default:
+		fmt.Println("switch")
+	}
+
 }
