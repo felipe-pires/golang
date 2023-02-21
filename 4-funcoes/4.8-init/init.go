cf3d5769b8dbc1ebd16c259589857bd8dff8c963
Felipe dos Santos <felipedossantospires01@gmail.com>
2023-02-21 14:40:55 -0300

    func init

--- 4-funcoes/4.8-init/init.go
+++ 4-funcoes/4.8-init/init.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 //executa antes da main
-func init() {
+func init() { //uma por arquivo
 	fmt.Println("init")
 }
 
-func main() {
+func main() { //uma por pacote
 	fmt.Println("main")
 }
