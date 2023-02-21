076d0502f3ac5ab3953537c6f401f1ec92dce017
Felipe dos Santos <felipedossantospires01@gmail.com>
2023-02-21 13:12:32 -0300

    panic e recover

---
+++ 4-funcoes/4.6-panic-recover/panicRecover.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(test_panic(5, 5))
+	fmt.Println("Pos execucao")
+}
+
+func test_panic(n1, n2 float64) bool {
+	defer test_recover()
+	media := (n1 + n2) / 2
+
+	if media > 5 {
+		return true
+	} else if media < 5 {
+		return false
+	}
+	panic("media 6")
+}
+
+func test_recover() {
+	if r := recover(); r != nil {
+		fmt.Println("test recover")
+	}
+}
